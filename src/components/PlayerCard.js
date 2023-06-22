5f0a58472eb216f4d84503c734e833fa13c8bd66
William Baele <baelewilliam1999@gmail.com>
2023-06-22 16:31:45 +0200

    UI improvements

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -54,7 +54,7 @@ const PlayerCard = ({ name, numAffilie }) => {
               <div class="bg-gray-100 shadow sm:rounded-lg ">
                 <div class="px-4 py-5 sm:px-6">
                   <h2 class="text-2xl leading-6 font-medium text-gray-900">
-                    Information du joueur
+                    Informations du joueur
                   </h2>
                 </div>
                 <div class="border-t border-gray-200 px-4 py-5 sm:px-6">
@@ -110,10 +110,20 @@ const PlayerCard = ({ name, numAffilie }) => {
           </div>
           <section class="lg:col-start-3 lg:col-span-1">
             <div class="bg-gray-100  shadow sm:rounded-lg ">
-              <div class="px-4 py-5 sm:px-6">
+              <div class="px-4 py-5 sm:px-6 flex justify-between">
                 <h2 class="text-2xl leading-6 font-medium text-gray-900">
                   Résultats récents
                 </h2>
+                <div className="w-28 rounded-xl flex">
+                  <div
+                    className="h-full rounded-l-xl bg-green-600"
+                    style={{ width: "66.66%" }}
+                  ></div>
+                  <div
+                    className="h-full rounded-r-xl bg-red-600"
+                    style={{ width: "33.33%" }}
+                  ></div>
+                </div>
               </div>
               <div class="border-t border-gray-200 ">
                 <ul class="p-6">
@@ -230,16 +240,16 @@ const PlayerCard = ({ name, numAffilie }) => {
                       ></span>
                       <div class="relative flex space-x-3">
                         <div>
-                          <span class="h-6 w-6 rounded-full bg-green-600 flex items-center justify-center">
+                          <span class="h-6 w-6 rounded-full bg-red-600 flex items-center justify-center">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               width="16"
                               height="16"
                               fill="#FFFFFF"
-                              class="bi bi-check-lg"
+                              class="bi bi-x-lg"
                               viewBox="0 0 16 16"
                             >
-                              <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
+                              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
                             </svg>
                           </span>
                         </div>
